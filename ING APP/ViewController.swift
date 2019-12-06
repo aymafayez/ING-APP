8f36d0c288bf1c59eb38d7f30c22e103a6227ba1
Ayman Fayez <aymanfayez277@gmail.com>
2019-12-06 11:25:25 +0200

    Initial Commit

---
+++ ING APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ING APP
+//
+//  Created by Guest2 on 12/6/19.
+//  Copyright © 2019 ING. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

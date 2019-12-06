dd359a3790a0e8fea615f8da5b3f37041a965960
Ayman Fayez <aymanfayez277@gmail.com>
2019-12-06 12:30:20 +0200

    Marketplace home cell design is added

---
+++ ING APP/Marketplace/Home/View/MarketplaceHomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MarketplaceHomeTableViewCell.swift
+//  ING APP
+//
+//  Created by Guest2 on 12/6/19.
+//  Copyright © 2019 ING. All rights reserved.
+//
+
+import UIKit
+
+class MarketplaceHomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
